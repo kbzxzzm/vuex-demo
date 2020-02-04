ceb3e386a01f16c76c16b50daaf09e9bbb53db88
周哲名 <zhouzheming@zhouzhemingdeMacBook-Pro.local>
2020-02-04 19:38:39 +0800

    vuex浅知

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,33 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h1>vuex中的state值{{ this.$store.state.count }}</h1>
+    <h2>vuex中的getters值{{ this.$store.getters.getstate }}</h2>
+    <button @click="addcount">+</button>
+    <button @click="reducecount">-</button>
+    <h3>我是mapstatecount=>{{ count1 }}</h3>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-
+import { mapState, mapActions, mapGetters } from "vuex";
 export default Vue.extend({
-  name: "HelloWorld",
-  props: {
-    msg: String
+  computed: {
+    ...mapState({
+      count1: function(state) {
+        return this.$store.state.count; //相当于展开运算符，简化vuex一长串写法
+      }
+    })
+  },
+  methods: {
+    addcount() {
+      var somebody = "我是后加的";
+      // this.$store.commit('add')//vue是不建议我们这样写的
+      this.$store.dispatch("addactions", somebody); //效果是一样的。代码是在冗余
+    },
+    reducecount() {
+      this.$store.dispatch("redactions");
+    }
   }
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,30 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 1
+  },
+  getters: {
+    getstate: function(state) {
+      return state.count + 1; //getters相当于vue的computed计算属性
+    }
+  },
+  mutations: {
+    add(state, somebody) {
+      state.count = state.count + 1 + somebody;
+    },
+    reduce(state) {
+      state.count = state.count - 1;
+    }
+  },
+  actions: {
+    //注册actions相当于vue的methods
+    addactions(context, somebody) {
+      context.commit("add", somebody); //当然，很冗余
+    },
+    redactions(context) {
+      context.commit("reduce");
+    }
+  },
   modules: {}
 });
